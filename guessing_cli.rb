11ac58a615b6eb1243fcf8797b4028ef0da4db97
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 20:28:24 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   input = gets.chomp
   if input == "exit"
   puts "Goodbye!"
-  exit
+  
   elsif input.to_i == computer_number
   puts "You guessed the correct number!"
 else
